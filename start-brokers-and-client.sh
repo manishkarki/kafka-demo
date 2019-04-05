480abfd2f3862a9ac952ab5280933aae0e745473
Manish Karki <manishkarki@USMBP.local>
2019-04-04 20:44:53 -0400

    added the start-up script

---
+++ start-brokers-and-client.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -e
+
+#first create the network
+docker network create kafka
+#run the latest zookeeper image
+docker run --net=kafka -d --name=zookeeper -e ZOOKEEPER_CLIENT_PORT=2181 confluentinc/cp-zookeeper:4.1.0
+#run the kafka
+docker run --net=kafka -d -p 9092:9092 --name=kafka -e KAFKA_ZOOKEEPER_CONNECT=zookeeper:2181 -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://kafka:9092 -e KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR=1 aa982a32e7c4
+
+
